484ba7d35a28a9d66adf6a89620b379fd1b5566e
yfe404 <yfe.github@protonmail.com>
2025-06-20 17:56:59 +0200

    task4

---
+++ uploads/task04/shell_exec.s
@@ -0,0 +1,18 @@
+
+section .data
+    msg db '/bin/sh'
+
+section .text
+    global _start
+
+_start:
+
+    mov rax, 59                    ; syscall number 1 = sys_write, 59 execve
+    mov rdi, msg                    ; pointer to filename
+    mov rsi, 0                  ; pointer to argv
+    mov rdx, 0                   ; pointer to envp
+    syscall                      ; make kernel call
+
+    mov eax, 60       ; syscall number for exit
+    mov edi, 4242       ; exit code
+    syscall
